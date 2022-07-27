2001feda3344460666ee888780b0cc393da3740d
mary-renjith <mary.renjith@gmail.com>
2022-07-27 09:33:18 +0530

    chnages cache

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  postLogoutUrl:"https://mary-renjith.github.io/aad-auth-angulardemo"
+  postLogoutUrl:"https://mary-renjith.github.io/aad-auth-angulardemo/"
 };
